b957eb3cad9adf56418f864e6f60b3af21391379
duongthing <62350848+duongthing@users.noreply.github.com>
2020-06-01 11:31:00 +0700

    delete

--- routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var router  = express.Router()
-var controller = require('../controller/auth.controller')
-
-router.get('/login', controller.login);
-router.post('/login', controller.postLogin);
-router.get('/logout',controller.logout);
-
-module.exports = router;
